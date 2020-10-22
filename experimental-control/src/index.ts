c21ecd9cf22f554228127accc6051d28f779bc29
Andrew Olney <aolney@memphis.edu>
2020-10-22 13:56:56 -0500

    removed timer, trigger with notebook load

--- experimental-control/src/index.ts
+++ experimental-control/src/index.ts
@@ -3,28 +3,36 @@ import {
   JupyterFrontEndPlugin,
 } from "@jupyterlab/application";
 
+import {
+  INotebookTracker
+} from '@jupyterlab/notebook';
+
+// Hide UI elements to effect lockdown
+function lockdown() {
+
+    document.getElementById("jp-top-panel").style.display = "none";
+    document.getElementById("jp-left-stack").style.display = "none";
+
+    let mainPanelFirstChild = document.getElementById("jp-main-content-panel").children[0];
+    (mainPanelFirstChild as HTMLElement).style.display = "none";
+
+  }
+
 /**
  * Initialization data for the experimental-control extension.
  */
 const extension: JupyterFrontEndPlugin<void> = {
   id: "experimental-control",
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    setTimeout(() => {
-      const urlParams = new URLSearchParams(window.location.search);
-      const lockParam = urlParams.get("lock");
-      if (lockParam === "1") {
-        document.getElementById("jp-top-panel").style.display = "none";
-        document.getElementById("jp-left-stack").style.display = "none";
-
-        let mainPanelFirstChild = document.getElementById(
-          "jp-main-content-panel"
-        ).children[0];
-        (mainPanelFirstChild as HTMLElement).style.display = "none";
-      }
-    }, 1000);
-    console.log("JupyterLab extension experimental-control is activated!");
-  },
+  requires: [INotebookTracker],
+  activate: (app: JupyterFrontEnd, notebooks: INotebookTracker) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const lockParam = urlParams.get("lock");
+    if (lockParam === "1") {
+      console.log("JupyterLab extension experimental-control is activated!");
+      notebooks.currentChanged.connect( lockdown, null)
+    }
+  }
 };
 
 export default extension;
